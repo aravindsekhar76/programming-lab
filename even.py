ac86b4cdd49f996993827b31cb0a07a9340178f9
aravindsekhar76 <115882981+aravindsekhar76@users.noreply.github.com>
2023-03-01 08:09:55 +0530

    Update even.py

--- even.py
+++ even.py
@@ -2,5 +2,5 @@ a=int(input("enter a number: "))
 if(a%2==0):
         print("the given number is even")
 else:
-        print("the given number is odd")
+        print("the given number is odd"
       
